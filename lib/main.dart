5bffbde48817d45b44743f72a7a68638b871da59
Panutsaya-Rujakom <mymint1598@gmail.com>
2021-11-18 17:26:57 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:app/src/app.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
+
+//mint is test
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
